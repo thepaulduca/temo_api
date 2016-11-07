d6a4d3e8e8bf0a23300b11802abac423de475b1d
Paul Duca <paul.r.duca@gmail.com>
2016-11-07 14:13:38 -0500

    added route

--- temo_api/config/routes.rb
+++ temo_api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:new, :create, :show]
     resources :conversations, only: [:create, :show]
     resources :messages, only: [:create]
 end
