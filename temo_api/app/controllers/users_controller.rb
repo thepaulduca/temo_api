bf1060de9477ff7dd902b6b0ab621881b1645334
Paul Duca <paul.r.duca@gmail.com>
2016-11-07 16:52:53 -0500

    ya ya

--- temo_api/app/controllers/users_controller.rb
+++ temo_api/app/controllers/users_controller.rb
@@ -4,23 +4,20 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # render json: @user, status: :create
-      render json: {status: => "true"}
+      render json: @user, status: :create
+      # render json: {status: "true"}
     else
-      # render json: { errors: @user.errors }, status: :unprocessable_entity
-      render json: {status: => "false"}
+      render json: { errors: @user.errors }, status: :unprocessable_entity
+      # render json: {status: "false"}
     end
   end
 
   def show
     @user = User.find_by(phone: params[:phone])
     if @user
-      # render json: @user, status: :ok
-      render json: {status: "true"}
-
+      render json: @user, status: :ok
     else
-      # render json: { errors: @user.errors }, status: :unprocessable_entity
-      render json: {status: "false"}
+      render json: { errors: @user.errors }, status: :unprocessable_entity
 
     end
   end
