5634a04f783adec1ac57c0cfee2770c0b49e2fbc
Paul Duca <paul.r.duca@gmail.com>
2016-11-07 13:55:09 -0500

    delete files

--- app/controllers/conversation_controller.rb
+++
@@ -1,6 +0,0 @@
-class ConversationController < ApplicationController
-
-  def create
-    @conversation = Conversation.new(users: params[:users], sentiment: params[:sentiment])
-  end
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_many :messages
-  has_many :conversations
-end
