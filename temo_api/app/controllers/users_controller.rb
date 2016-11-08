efaf16e32e5afe47d725e8bd57a8e22386658920
Paul Duca <paul.r.duca@gmail.com>
2016-11-08 17:50:18 -0500

    fix

--- temo_api/app/controllers/users_controller.rb
+++ temo_api/app/controllers/users_controller.rb
@@ -20,58 +20,6 @@ class UsersController < ApplicationController
     render json: @contact_array, status: :ok
   end
 
-  # def show
-  #   @user = User.find_by(phone: normalize_phone_number(params[:phone]))
-  #   if @user
-  #     render json: @user, status: :ok
-  #   else
-  #     render json: { errors: 'Error!' }, status: :unprocessable_entity
-  #   end
-  # end
-  #
-  #
-  # {"contacts"=>
-  #   [{"givenName"=>"John",
-  #     "thumbnailPath"=>"",
-  #     "phoneNumbers"=>[{"number"=>"888-555-5512", "label"=>"mobile"}, {"number"=>"888-555-1212", "label"=>"home"}],
-  #     "familyName"=>"Appleseed",
-  #     "emailAddresses"=>[{"email"=>"John-Appleseed@mac.com", "label"=>"work"}],
-  #     "recordID"=>3},
-  #
-  #     {"givenName"=>"Kate",
-  #       "thumbnailPath"=>"",
-  #       "phoneNumbers"=>[{"number"=>"(555) 564-8583", "label"=>"mobile"}, {"number"=>"(415) 555-3695", "label"=>"main"}],
-  #       "familyName"=>"Bell",
-  #       "emailAddresses"=>[{"email"=>"kate-bell@mac.com", "label"=>"work"}, {"email"=>"www.icloud.com", "label"=>"work"}],
-  #       "recordID"=>1},
-  #
-  #       {"givenName"=>"Anna",
-  #         "thumbnailPath"=>"",
-  #         "phoneNumbers"=>[{"number"=>"555-522-8243", "label"=>"home"}],
-  #         "familyName"=>"Haro",
-  #         "emailAddresses"=>[{"email"=>"anna-haro@mac.com", "label"=>"home"}], "recordID"=>4},
-  #
-  #
-  #         {"givenName"=>"Daniel",
-  #           "thumbnailPath"=>"",
-  #           "phoneNumbers"=>[{"number"=>"555-478-7672", "label"=>"home"}, {"number"=>"(408) 555-5270", "label"=>"mobile"}, {"number"=>"(408) 555-3514", "label"=>"home fax"}],
-  #           "familyName"=>"Higgins", "emailAddresses"=>[{"email"=>"d-higgins@mac.com", "label"=>"home"}], "recordID"=>2},
-  #
-  #           {"givenName"=>"David", "thumbnailPath"=>"",
-  #             "phoneNumbers"=>[{"number"=>"555-610-6679", "label"=>"home"}],
-  #           "familyName"=>"Taylor",
-  #           "emailAddresses"=>[], "recordID"=>6}, {"emailAddresses"=>[{"email"=>"hank-zakroff@mac.com", "label"=>"work"}],
-  #
-  #
-  #           "middleName"=>"M.", "familyName"=>"Zakroff", "givenName"=>"Hank", "thumbnailPath"=>"",
-  #           "phoneNumbers"=>[{"number"=>"(555) 766-4823", "label"=>"work"}, {"number"=>"(707) 555-1854", "label"=>"other"}], "recordID"=>5}],
-  #
-  #
-  #
-  #           "user"=>{}}
-
-
-
 
  private
 
