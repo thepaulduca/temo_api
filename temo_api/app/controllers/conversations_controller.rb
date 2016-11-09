5f7def05c9b02ac3678b73e3fd6b52275118c0ea
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 11:29:40 -0500

    update create method

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -3,20 +3,20 @@ class ConversationsController < ApplicationController
   def create
     puts '--------------------------------------------'
     puts 'params'
-    puts params
-    puts 'get user name - with get username method being called'
-    puts get_username(params[:userone])
-    puts 'user one instance'
-    puts @user_1.inspect
+    puts params.inspect
+    puts 'user_one'
+    user_one = params[:user_one]
+    user_one_augment = get_username(user_one)
+    puts user_one_augment
     puts '--------------------------------------------'
-    user_1 = User.find_by(username: get_username(params[:userone]))
-    @user_2 = User.find_by(phone: params[:phone])
-    @conversation = Conversation.new(channel_url: params[:channel_url])
+    user_1 = User.find_by(username: get_username(user_one))
+    @user_2 = User.find_by(phone: params[:user_two])
+    @conversation = Conversation.new(channel_url: params[:channel_url][:url])
     @conversation.users = [@user_1, @user_2]
     if @conversation.save
-      render json: {status: "true"}
+      render json: {worked: "true"}
     else
-      render json: {status: "false"}
+      render json: {worked: "false"}
     end
   end
 
@@ -27,7 +27,7 @@ class ConversationsController < ApplicationController
   end
 
   def get_username(guest_username)
-    guest_username.slice!(7..-1)
+    guest_username.slice!(6 ..-1)
   end
 
 end
