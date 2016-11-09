de405250e5283d40a8a33671c4dbc0b389aff295
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 12:25:50 -0500

    user check

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -3,11 +3,11 @@ class ConversationsController < ApplicationController
   def create
     user_one = params[:user_one]
     user_one_augment = get_username(user_one)
-    @user_1 = User.find_by(username: user_one_augment)
+    @user_1 = User.find_by(username: 'paul')
     puts '*' * 100
     puts 'I AM USER ONE'
     puts @user_1
-    @user_2 = User.find_by(phone: params[:user_two])
+    @user_2 = User.find_by(phone: normalized_phone_number(params[:user_two]))
     puts '*' * 100
     puts 'I AM USER Two'
     puts @user_2
@@ -24,6 +24,10 @@ class ConversationsController < ApplicationController
   end
 
   private
+
+  def normalized_phone_number(phone_number)
+   phone_number.gsub(/[^\d]/,'')
+  end
 
   def conversation_params
     params.require(:conversation).permit(:userone, :phone, :channel_url, :sentiment)
