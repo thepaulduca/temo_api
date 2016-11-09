7c7ebad99e6068c11ab4d6fa232b72d503658880
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 13:41:33 -0500

    userID

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -5,7 +5,7 @@ class ConversationsController < ApplicationController
     puts '*' * 100
     puts 'params - conversation - userID'
     puts params['conversation']['userId']
-    @user_1 = User.find_by(phone: params['conversation']['userId'])
+    @user_1 = User.find_by(phone: params['conversation']['channel_url']['operators'][0]['userId'])
     puts @user_1
     @user_2 = User.find_by(phone: normalized_phone_number(params[:user_two]))
     @conversation = Conversation.new(channel_url: params[:channel_url][:url])
@@ -35,3 +35,16 @@ class ConversationsController < ApplicationController
   end
 
 end
+
+
+
+# {"channel_url"=>
+#   {"channelType"=>"open", "participantCount"=>0, "operators"=> [{"nickname"=>"Guest_VVNWKW", "profileUrl"=>"https://sendbird.com/main/img/profiles/profile_32_512px.png", "userId"=>"218460156330197502352", "lastSeenAt"=>0, "connectionStatus"=>"nonavailable"}],
+#     "url"=>"sendbird_open_channel_10291_812a8e60a422a5fe9153f2a42e7ed4ccbcad4366", "name"=>"rand", "coverUrl"=>"https://sendbird.com/main/img/cover/cover_01.jpg", "createdAt"=>1478716159000, "data"=>""},
+#
+#    "user_one"=>"Guest_VVNWKW", "user_two"=>"(555) 564-8583",
+#    params['conversation']['channel_url']['operators'][0]['userId']
+#     "conversation"=>{"channel_url"=>
+#       {"channelType"=>"open", "participantCount"=>0,
+#         "operators"=>[{"nickname"=>"Guest_VVNWKW", "profileUrl"=>"https://sendbird.com/main/img/profiles/profile_32_512px.png", "userId"=>"218460156330197502352", "lastSeenAt"=>0, "connectionStatus"=>"nonavailable"}],
+#         "url"=>"sendbird_open_channel_10291_812a8e60a422a5fe9153f2a42e7ed4ccbcad4366", "name"=>"rand", "coverUrl"=>"https://sendbird.com/main/img/cover/cover_01.jpg", "createdAt"=>1478716159000, "data"=>""}}}
