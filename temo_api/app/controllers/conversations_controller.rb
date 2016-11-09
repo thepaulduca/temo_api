7896c79c6a88b559e1c76523e8d8d1968d060860
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 12:13:17 -0500

    print users

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -3,18 +3,18 @@ class ConversationsController < ApplicationController
   def create
     user_one = params[:user_one]
     user_one_augment = get_username(user_one)
-    puts "*" * 30
-    puts "i'm the params"
-    puts params
-    puts "*" * 30
-    puts ''
-    puts "*" * 30
-    puts "I'm the user one argument"
-    puts user_one_augment
-    puts "*" * 30
     @user_1 = User.find_by(username: user_one_augment)
+    puts '*' * 100
+    puts 'I AM USER ONE'
+    puts @user_1
     @user_2 = User.find_by(phone: params[:user_two])
+    puts '*' * 100
+    puts 'I AM USER Two'
+    puts @user_2
     @conversation = Conversation.new(channel_url: params[:channel_url][:url])
+    puts '*' * 100
+    puts 'I AM CONVERSATION'
+    puts @conversation
     @conversation.users = [@user_1, @user_2]
     if @conversation.save
       render json: {worked: "true"}
