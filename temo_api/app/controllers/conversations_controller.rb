d9c3e6e8df8de4dec8807e4814a5976b148985fa
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 12:02:22 -0500

    find out user one params

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -1,15 +1,18 @@
 class ConversationsController < ApplicationController
 
   def create
-    puts '--------------------------------------------'
-    puts 'params'
-    puts params.inspect
-    puts 'user_one'
     user_one = params[:user_one]
     user_one_augment = get_username(user_one)
+    puts "*" * 30
+    puts "i'm the params"
+    puts params
+    puts "*" * 30
+    puts ''
+    puts "*" * 30
+    puts "I'm the user one argument"
     puts user_one_augment
-    puts '--------------------------------------------'
-    @user_1 = User.find_by(username: get_username(user_one))
+    puts "*" * 30
+    @user_1 = User.find_by(username: user_one_augment)
     @user_2 = User.find_by(phone: params[:user_two])
     @conversation = Conversation.new(channel_url: params[:channel_url][:url])
     @conversation.users = [@user_1, @user_2]
