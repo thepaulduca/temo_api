1d66a6b9b93a3fdd4edbd49030ba9250583321af
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 11:31:44 -0500

    fix typo

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -9,7 +9,7 @@ class ConversationsController < ApplicationController
     user_one_augment = get_username(user_one)
     puts user_one_augment
     puts '--------------------------------------------'
-    user_1 = User.find_by(username: get_username(user_one))
+    @user_1 = User.find_by(username: get_username(user_one))
     @user_2 = User.find_by(phone: params[:user_two])
     @conversation = Conversation.new(channel_url: params[:channel_url][:url])
     @conversation.users = [@user_1, @user_2]
