6a3a7ca052bc32b51e6bf4d6aa276ff83b635970
Paul Duca <paul.r.duca@gmail.com>
2016-11-09 23:06:46 -0500

    fix

--- temo_api/app/controllers/conversations_controller.rb
+++ temo_api/app/controllers/conversations_controller.rb
@@ -8,7 +8,7 @@ class ConversationsController < ApplicationController
       @conversation = Conversation.new(channel_url: params[:channel_url][:url], users: [@user_1, @user_2])
       if @conversation.save
         @conversation.users
-        render json: @conversations
+        render json: {worked: "true"}
       else
         render json: {worked: "false"}
       end
